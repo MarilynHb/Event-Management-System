d8cbeb89531208d39250da0f8c29e8b55dbe1104
Marilyn <mh.marilynhaber@gmail.com>
2024-06-08 18:42:11 +0300

    remove unused imports

--- Ang/src/app/app.module.ts
+++ Ang/src/app/app.module.ts
@@ -4,10 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddUsersComponent } from './add-users/add-users.component';
-import { AddEventComponent } from './components/add-event/add-event.component';
-import { EventDetailsComponent } from './components/event-details/event-details.component';
-import { EventsListComponent } from './components/events-list/events-list.component';
+// import { AddUsersComponent } from './add-users/add-users.component';
+// import { AddEventComponent } from './components/add-event/add-event.component';
+// import { EventDetailsComponent } from './components/event-details/event-details.component';
+// import { EventsListComponent } from './components/events-list/events-list.component';
 import { LocationShowComponent } from './location-show/location-show.component';
 import { EventTypeShowComponent } from './event-type-show/event-type-show.component';
 import { EventTagShowComponent } from './event-tag-show/event-tag-show.component';
@@ -15,10 +15,10 @@ import { EventTagShowComponent } from './event-tag-show/event-tag-show.component
 @NgModule({
   declarations: [
     AppComponent,
-    AddUsersComponent,
-    AddEventComponent,
-    EventDetailsComponent,
-    EventsListComponent,
+    // AddUsersComponent,
+    // AddEventComponent,
+    // EventDetailsComponent,
+    // EventsListComponent,
     LocationShowComponent,
     EventTypeShowComponent,
     EventTagShowComponent,
